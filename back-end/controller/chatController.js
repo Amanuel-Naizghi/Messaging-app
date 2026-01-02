38e8d79b2f25827321f3c0b85ddf856d17446f4b
Amanuel-Naizghi <naizghiaman@gmail.com>
2026-01-02 15:40:57 -0700

    Modules for creating a new chat and sending message has been created

--- back-end/controller/chatController.js
+++ back-end/controller/chatController.js
@@ -22,3 +22,53 @@ exports.getUserChats = async (req,res) =>{
 
     res.json(chats);
 }
+
+exports.createChat = async (req,res) => {
+    const userId = req.user.id;
+    const {membersIds} = req.body;
+
+    if(!Array.isArray(membersIds) || membersIds.length === 0){
+        return res.status(400).json({error:"messageId required"});
+    }
+
+    try{
+        const chat = await prisma.chat.create({
+            data: {
+                members: {
+                    create: [
+                        {userId},
+                        ...membersIds.map(id => ({userId:id}))
+                    ]
+                }
+            }
+        });
+
+        res.status(201).json(chat);
+    }catch(err){
+        res.status(500).json({error: err.message})
+    }
+}
+
+exports.sendMessage = async (req,res) => {
+    const userId = req.user.id;
+    const {content} = req.body;
+    const {chatId} = req.params;
+
+    const isMember = await prisma.chatMember.findFirst({
+        where:{chatId:Number(chatId), userId}
+    })
+    
+    if(!isMember){
+        return res.status(403).json({error: "Not a member"})
+    }
+
+    const message = await prisma.message.create({
+        data: {
+            chatId:Number(chatId),
+            senderId:userId,
+            content
+        }
+    });
+
+    res.status(201).json(message);
+}
--- back-end/router/userRouter.js
+++ back-end/router/userRouter.js
@@ -30,5 +30,7 @@ router.get('/success',ensureAuthenticated,(req,res) => {
 })//Used just for testing purpose for login
 
 router.get('/chats',ensureAuthenticated, chatController.getUserChats);//We used /chats as an API call for getting all messages by a user
+router.post('/chats',ensureAuthenticated, chatController.createChat);// For creating a new conversations for a group or 1 to 1
+router.post('/chats/:chatId/messages',ensureAuthenticated, chatController.sendMessage);//For sending a new message
 
 module.exports = router;

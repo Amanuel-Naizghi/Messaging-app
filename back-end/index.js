d6e8f92643257aa9de933ac636e62ffe6059afe1
amanuel-naizghi <naizghiaman@gmail.com>
2026-07-20 00:19:53 -0600

    cleaned up some testing code from index.js for deployment purpose

--- back-end/index.js
+++ back-end/index.js
@@ -2,23 +2,5 @@ const {PrismaClient} = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
-async function main() {
-    // const newUser = await prisma.user.create({
-    //     data: {
-    //         userName : 'Moss',
-    //         password: 'Moss123$',
-    //     },
-    // });
-
-    // console.log('Created user:',newUser);
-
-    const foundUser = await prisma.user.findMany();
-
-    // console.log('Found user:', foundUser);
-}
-
-main()
-    .catch((e) => console.error(e))
-    .finally(() => prisma.$disconnect());
 
 module.exports = prisma;
